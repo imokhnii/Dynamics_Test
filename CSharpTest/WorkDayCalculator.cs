9150da06c441c4e1f56743f49a75b14b7cde7249
imokhnii <officialmokhnii@gmail.com>
2019-10-20 16:27:33 +0300

    Finished task

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpTest
+{ 
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            DateTime endDate = startDate.AddDays(dayCount - 1);
+
+            if (weekEnds != null)
+            {
+                foreach (WeekEnd vacationRange in weekEnds)
+                {
+                    if (vacationRange.StartDate >= startDate && vacationRange.StartDate <= endDate)
+                    {
+                        endDate =  endDate.AddDays((vacationRange.EndDate - vacationRange.StartDate).Days + 1);
+                    }
+                }
+            }
+
+            return endDate;
+        }
+    }
+}
